6744f5e166288dcb80979971c05932c66d8c6670
Tyl <tyldahl@proton.me>
2026-07-01 20:14:03 -0400

    Add aliases

---
+++ .config/shell/aliases.sh
@@ -0,0 +1,8 @@
+alias ls="ls -h --color=auto --group-directories-first"
+alias ll="ls -lh --color=auto --group-directories-first"
+alias la="ls -lah --color=auto --group-directories-first"
+
+alias c="clear"
+alias fd="fdfind"
+
+alias config="git --git-dir=$HOME/dotfiles.git --work-tree=$HOME"
