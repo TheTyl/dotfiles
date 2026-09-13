d3ad94c43f8224ec26864e66184a508b3aeb88fb
Tyl <tyldahl@proton.me>
2026-09-12 22:09:39 -0400

    Add nvim

---
+++ .config/nvim/lua/keymaps.lua
@@ -0,0 +1,30 @@
+vim.keymap.set("i", "jj", "<esc>", {remap = false})
+
+vim.keymap.set({"n", "v", "i"}, "<up>", "<nop>")
+vim.keymap.set({"n", "v", "i"}, "<down>", "<nop>")
+vim.keymap.set({"n", "v", "i"}, "<left>", "<nop>")
+vim.keymap.set({"n", "v", "i"}, "<right>", "<nop>")
+
+vim.keymap.set("n", "<C-k>", "<cmd>m-2<cr>")
+vim.keymap.set("n", "<C-j>", "<cmd>m+<cr>")
+
+vim.keymap.set("n", "<leader>b", "^")
+vim.keymap.set("n", "<leader>e", "$")
+
+vim.keymap.set("n", "-", "<cmd>Ex<cr>")
+
+vim.keymap.set("n", "<leader>w", "<C-w>w")
+vim.keymap.set("n", "<leader>cn", "<cmd>cnext<cr>")
+vim.keymap.set("n", "<leader>cp", "<cmd>cprev<cr>");
+
+-- fzf-lua
+vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", {desc="Find files"})
+vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>", {desc="Find grep"})
+vim.keymap.set("n", "<leader>fc", "<cmd>lua require('fzf-lua').files({cwd=vim.fn.stdpath('config')})<cr>", {desc="find in neovim configuration"})
+vim.keymap.set("n", "<leader>fh", "<cmd>FzfLua helptags<cr>", {desc="Find help"})
+vim.keymap.set("n", "<leader>fw", "<cmd>FzfLua grep_cword<cr>", {desc="Find current word"})
+vim.keymap.set("n", "<leader>fW", "<cmd>FzfLua grep_cWORD<cr>", {desc="Find current WORD"})
+vim.keymap.set("n", "<leader>fr", "<cmd>FzfLua resume<cr>", {desc="Find resume"})
+vim.keymap.set("n", "<leader>fo", "<cmd>FzfLua oldfiles<cr>", {desc="Find old files"})
+vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", {desc="Find existing buffers"})
+vim.keymap.set("n", "<leader>/", "<cmd>FzfLua lgrep_curbuf<cr>", {desc="Live grep the current buffer"})
---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,33 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.confirm = true
+vim.opt.wrap = false
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+vim.opt.guicursor = ""
+vim.opt.background = "dark"
+vim.opt.scrolloff = 12
+vim.opt.showmode = false
+
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+vim.opt.undofile = true
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.mouse = "a"
+
+vim.opt.clipboard:append "unnamedplus"
